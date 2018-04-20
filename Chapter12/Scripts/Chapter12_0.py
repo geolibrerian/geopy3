90ee089a289ec93dde8c89a4be6cdfdd072167b6
geolibrerian <silasmerlin@gmail.com>
2018-04-20 12:48:33 -0700

    adjusted database creation in chapter 12 again

--- Chapter12/Scripts/Chapter12_0.py
+++ Chapter12/Scripts/Chapter12_0.py
@@ -1,10 +1,16 @@
 import psycopg2
-connection = psycopg2.connect(host='localhost', user='postgres',password='g30d@t@', port="5432")
+connection = psycopg2.connect(host='localhost', user='{user}',
+	password='{password}', port="5432")
 connection.autocommit = True
 cursor = connection.cursor()
 cursor.execute('CREATE DATABASE chapter122')
-
-cursor.execute('CREATE EXTENSION postgis')
 connection.close() 
 
+
+connection = psycopg2.connect(dbname='chapter12', host='localhost',
+ user='{user}', password='{password}', port="5432")
+cursor = connection.cursor()
+connection.autocommit = True
+cursor.execute('CREATE EXTENSION postgis')
+connection.close() 
 
